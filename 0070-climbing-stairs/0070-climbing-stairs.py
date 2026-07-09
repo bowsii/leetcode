38a050eca5e8378891758ee0a3764d2afec20d52
Bowsickdharan <bowsickdharan@gmail.com>
2026-07-09 14:32:40 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.97%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=3 :
+            return n
+        p1=3
+        p2=2
+        c=0
+        for _ in range(3,n):
+            c=p1+p2
+            p2=p1
+            p1=c
+        return c
